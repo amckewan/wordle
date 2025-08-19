2d6b0bbd6da82564dfbc7e668c1f24ed2c7c9e9b
Andrew McKewan <andrew.mckewan@gmail.com>
2025-08-19 06:31:22 -0400

    Factor search

--- wordle.fs
+++ wordle.fs
@@ -1,6 +1,6 @@
 ( Wordle solver )
 
-( Build an array of valid Wordle words, 5 chars per entry )
+( 1. Build an array of valid Wordle words, 5 chars per entry )
 
 5 CONSTANT LEN
 
@@ -10,34 +10,55 @@ CREATE WORDLE-WORDS   INCLUDE wordle-words.fs
 
 HERE WORDLE-WORDS - LEN / CONSTANT #WORDS
 
-: WW ( n - a)  LEN * WORDLE-WORDS + ;
+: WW ( n -- a )  LEN * WORDLE-WORDS + ;
 
-: .WW ( n -)  WW LEN TYPE SPACE ;
+: .WW ( n -- )  WW LEN TYPE SPACE ;
 
 ( show all the words )
 : .WORDS  #WORDS 0 DO  I .WW  LOOP ;
 
-( lookup a word using binary search )
+
+( 2. Lookup a word using binary search )
 : WW? ( a n -- false | w# true )
-  5 <> IF DROP FALSE EXIT THEN  >R  0 #WORDS ( low high )
+  LEN <> IF DROP FALSE EXIT THEN  >R  0 #WORDS ( low high )
   BEGIN 2DUP < WHILE
     2DUP + 2/ ( low high mid )
     DUP WW  R@ LEN  ROT LEN COMPARE
     DUP 0= IF ( found ) R> 2DROP NIP NIP TRUE EXIT  THEN
-    0< IF ( bottom half ) NIP  ELSE  ( top half ) ROT DROP  1+ SWAP  THEN
+    0< IF ( bottom half ) NIP  ELSE ( top half ) ROT DROP  1+ SWAP  THEN
   REPEAT
   2DROP R> DROP FALSE ;
 
-: TEST-WW?
+( with factoring )
+: MORE? ( low high -- f )  2DUP < ;
+: MID ( low high -- mid )  + 2/ ;
+: MATCH ( a a -- n )  LEN SWAP LEN COMPARE ;
+: FOUND ( low high mid -- mid true )    NIP NIP TRUE ;
+: LOWER ( low high mid -- low mid )     NIP ;
+: UPPER ( low high mid -- mid+1 high )  ROT DROP  1+ SWAP ;
+
+: WW2?  ( a n -- false | w# true )
+  LEN = IF  >R  0 #WORDS ( low high )
+  BEGIN MORE? WHILE
+    2DUP MID  R@ OVER WW MATCH
+    ?DUP 0= IF FOUND  R> DROP EXIT  THEN
+         0< IF LOWER  ELSE UPPER  THEN
+  REPEAT
+  2DROP R>  THEN DROP FALSE ;
+
+
+: (TEST-WW?)  ( xt -- )  >R
   ( valid words )
-  S" ABACK" WW? 0= ABORT" can't find ABACK" DROP
-  S" ABASE" WW? 0= ABORT" can't find ABASE" DROP
-  S" CADET" WW? 0= ABORT" can't find CADET" DROP
-  S" RANCH" WW? 0= ABORT" can't find RANCH" DROP
-  S" ZESTY" WW? 0= ABORT" can't find ZESTY" DROP
-  S" ZONAL" WW? 0= ABORT" can't find ZONAL" DROP
+  S" ABACK" R@ EXECUTE 0= ABORT" can't find ABACK" DROP
+  S" ABASE" R@ EXECUTE 0= ABORT" can't find ABASE" DROP
+  S" CADET" R@ EXECUTE 0= ABORT" can't find CADET" DROP
+  S" RANCH" R@ EXECUTE 0= ABORT" can't find RANCH" DROP
+  S" ZESTY" R@ EXECUTE 0= ABORT" can't find ZESTY" DROP
+  S" ZONAL" R@ EXECUTE 0= ABORT" can't find ZONAL" DROP
   ( invalid words )
-  S" XXXXX" WW? ABORT" found XXXXX"
-  S" ABAC" WW? ABORT" found ABAC"
-  S" ABACKX" WW? ABORT" found ABACKX" ;
-  
+  S" XXXXX" R@ EXECUTE ABORT" found XXXXX"
+  S" ABAC"  R@ EXECUTE ABORT" found ABAC"
+  S" ABACKX" R@ EXECUTE ABORT" found ABACKX"
+  R> DROP ;
+
+: TEST-WW?  ['] WW? (TEST-WW?)  ['] WW2? (TEST-WW?) ;
