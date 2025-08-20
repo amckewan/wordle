12edfa030f21e13f7dab3f36c3aa5c7efc374aa5
Andrew McKewan <andrew.mckewan@gmail.com>
2025-08-20 06:46:16 -0400

    Start guessing

--- wordle.fs
+++ wordle.fs
@@ -2,13 +2,5 @@
 
 include words.fs
 include set.fs
-
-\ current answer, char if green else 0
-create answer len allot
-: clear-answer  answer len erase ; clear-answer
-
-: green? ( pos -- f ) answer + c@ ;
-
-: .answer  len 0 do  i green?  ?dup 0= if [char] ? then emit  loop space ;
-
+include guess.fs
 include prune.fs
