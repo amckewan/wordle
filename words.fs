ec2ac35365375ae64291b5cd0696f02c5ad23272
Andrew McKewan <andrew.mckewan@gmail.com>
2025-08-21 15:01:55 -0400

    Improve search

--- words.fs
+++ words.fs
@@ -4,111 +4,103 @@
 
 \ Build an array of valid Wordle words, 5 chars per entry.
 \ Wordle words are identified by the index into this array.
-\ Use 'w' in stack diagrams.
+
+\ Wordle words are 5 characters long. When passing strings, we only need the address.
+\ This is 'wa' in stack diagrams (word address).
 
 5 CONSTANT LEN
 
+: .W ( w -- ) LEN TYPE SPACE ;
+
+: W ( -- w )  BL PARSE DROP ;
+: [W] ( -- w at runtime )  BL PARSE  POSTPONE SLITERAL  POSTPONE DROP ; IMMEDIATE
+
+\ Add the next wordle word to the dictionary (to build the word lists).
 : WW,  HERE LEN ALLOT  BL WORD COUNT  ( a' a # )  ROT SWAP CMOVE ;
 
-CREATE WORDLE-WORDS   INCLUDE wordle-words.fs
+\ Create the list of valid wordle solutions (sorted).
+\ In stack diagrams, a 'w' is a word number, which is an offset into this array. NO NO
+CREATE WORDLE-WORDS
+  INCLUDE wordle-words.fs
+HERE CONSTANT WORDLE-WORDS-END
 
-HERE WORDLE-WORDS - LEN / CONSTANT #WORDS
+WORDLE-WORDS-END WORDLE-WORDS - LEN / CONSTANT #WORDS
 
-\ Create the list of possible guess. These are allowed as guesses but won't be solutions.
-CREATE GUESSES  INCLUDE guess-words.fs
-HERE GUESSES - LEN / CONSTANT #GUESSES
+\ Create the list of possible guesses. These are allowed as guesses but won't be solutions.
+CREATE GUESS-WORDS
+  INCLUDE guess-words.fs
+HERE CONSTANT GUESS-WORDS-END
 
+GUESS-WORDS-END GUESS-WORDS - LEN / CONSTANT #GUESS-WORDS
 
-: ASSERT-WORD ( w -- w )  DUP #WORDS U>= ABORT" invalid wordle word" ;
-
-: WW ( w -- a )  LEN * WORDLE-WORDS + ;
+\ get string from word #
+: WW ( w -- wa )  LEN * WORDLE-WORDS + ;
 
 : .WW ( w -- )  WW LEN TYPE SPACE ;
 
 ( show all the words )
 : .WORDS  #WORDS 0 DO  I .WW  LOOP ;
 
-
-( 2. Lookup a word using binary search )
-: WORD? ( a n -- false | w true )
-  LEN <> IF DROP FALSE EXIT THEN
-  ( a ) >R  0 #WORDS ( low high )
-  BEGIN 2DUP < WHILE
-    2DUP + 2/ ( low high mid )
-    DUP WW  R@ LEN  ROT LEN COMPARE
-    DUP 0= IF ( found ) R> 2DROP NIP NIP TRUE EXIT  THEN
-    0< IF ( bottom half ) NIP  ELSE ( top half ) ROT DROP  1+ SWAP  THEN
-  REPEAT
-  2DROP R> DROP FALSE ;
-
-: MATCH ( a1 a2 -- n )  LEN SWAP LEN COMPARE ;
-
-\ Need to refactor so we can use it for both words and guesses
-: FIND-WORD ( a n words #words -- w true | false)
-  ROT LEN = IF  SWAP >R  0 SWAP ( a low high )
-    BEGIN 2DUP < WHILE
-      2DUP + 2/ ( a low high mid )
-      DUP LEN * R@ + ( a low high mid a' ) 4 PICK ( !) MATCH ( a low high mid f )
-      DUP 0= IF ( found ) R> 2DROP NIP NIP NIP  TRUE EXIT  THEN
-      0< IF ( bottom half ) NIP  ELSE ( top half ) ROT DROP  1+ SWAP  THEN
-    REPEAT
-    R> DROP
-  THEN 2DROP DROP FALSE ;
+\ : FOR-WORDS ( words n -- end begin )  LEN *  OVER + SWAP ;
 
 
-( with factoring )
-: MORE? ( low high -- f )  2DUP < ;
-: MID ( low high -- mid )  + 2/ ;
-: MATCH ( a a -- n )  LEN SWAP LEN COMPARE ;
-: FOUND ( low high mid -- mid true )    NIP NIP TRUE ;
-: LOWER ( low high mid -- low mid )     NIP ;
-: UPPER ( low high mid -- mid+1 high )  ROT DROP  1+ SWAP ;
+\ Search a sorted list of words, returning the word and true if found, else false.
+\ The w returned is in the table, not necessarily (and most likely not) the w passed in.
+\ We do that so the returned word is static and we can keep it around if needed.
 
-: WORD2?  ( a n -- false | w# true )
-  LEN = IF  >R  0 #WORDS ( low high )
-  BEGIN MORE? WHILE
-    2DUP MID  R@ OVER WW MATCH
-    ?DUP 0= IF FOUND  R> DROP EXIT  THEN
-         0< IF LOWER  ELSE UPPER  THEN
-  REPEAT
-  2DROP R>  THEN DROP FALSE ;
+: WCOMPARE ( w1 w2 -- -1/0/1 )  LEN SWAP LEN COMPARE ;
+: MID ( low high -- mid )  OVER - LEN / ( low n )  2/ LEN * + ;
+
+: FIND-WORD ( w words #words -- w' true | false)   ( binary search )
+    ROT >R  LEN * OVER + ( low high )
+    BEGIN 2DUP U< WHILE
+        2DUP MID  R@ OVER WCOMPARE ( low high mid n )
+        ?DUP 0= IF ( found ) NIP NIP TRUE   R> DROP EXIT THEN
+        0< IF ( bottom half ) NIP  ELSE ( top half ) ROT DROP  LEN + SWAP  THEN
+    REPEAT  2DROP FALSE   R> DROP ;
 
 
 ( === unit tests === )
 include unit-test.fs
 
-variable word-finder ( so we can try both )  ' word? word-finder !
-: check-valid  ( a n -- a n w t | a n f )  2dup word-finder @ execute ;
+create test-words
+WW, ABACK
+WW, BLANK
+WW, CHOIR
+WW, DELTA
+WW, EMCEE
+WW, FORTH
+WW, GRILL
+WW, HOUSE
+WW, IGLOO
+WW, JAPAN
+here test-words - len / constant #test-words
 
-: expect-valid ( a n -- )  test
-    check-valid 0= if  fail ." expect '" TYPE ." ' valid"
-    else  drop 2drop  then ;
-: expect-not-valid ( a n -- )  test
-    check-valid if  drop fail  ." expect '" TYPE ." ' not valid"
-    else  2drop  then ;
+\ : FIND-WORD ( w words #words -- w' true | false)
 
-: (test-word?) ( xt -- )  word-finder !
-  begin-unit-tests
-  s" ABACK" expect-valid
-  s" ABASE" expect-valid
-  s" CADET" expect-valid
-  s" RANCH" expect-valid
-  s" ZESTY" expect-valid
-  s" ZONAL" expect-valid
+: expect-found ( w -- )  test
+  dup test-words #test-words find-word
+  if 2drop else fail ." Expected to find " .W then ;
 
-\  ( these fail to test expect... )
-\  s" ZONAL" expect-not-valid
-\  s" XXXXX" expect-valid
+: expect-not-found ( w -- )  test
+  dup test-words #test-words find-word
+  if fail ." Expected not to find " drop .W else drop then ;
 
-  s" XXXXX" expect-not-valid
-  s" ABAC" expect-not-valid
-  s" ABACKX" expect-not-valid
+: test-find-word
+  CR ." Testing FIND-WORD..." begin-unit-tests
+  [W] ABACK expect-found
+  [w] BLANK expect-found
+  [w] CHOIR expect-found
+  [w] JAPAN expect-found
+  [w] ZESTY expect-not-found
+  [w] XXXXX expect-not-found
+
+  \ test an empty list
+  test [w] ABACK test-words 0 find-word
+  if fail ." Expect not to find in a empty table" drop then
 
   report-unit-tests ;
 
-: TEST-WORD?
-  cr ." Testing (WORD?)..."  ['] WORD?  (test-word?)
-  cr ." Testing (WORD2?)..." ['] WORD2? (test-word?) ;
+test-find-word
 
-test-word?
 forget-unit-tests
