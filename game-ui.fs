0edb1f0e6a60345e5dcf58083bbc21ae5aff943f
Andrew McKewan <andrew.mckewan@gmail.com>
2025-08-31 14:55:29 -0400

    Show used letter keyboard

--- game-ui.fs
+++ game-ui.fs
@@ -3,29 +3,30 @@
 : solved ( -- f )  #greens len = ;
 : failed ( -- f )  guesses @ #guesses >= ; ( assuming not solved )
 
-\ keyboard showing knowledge about each letter
+\ keyboard with knowledge about each letter played
+\ 0=unknown, 1=grey, 2=yellow, 3=green
 create kb 26 allot
 : kb@ ( c -- color )  A - kb + c@ ;
 : kb! ( color c -- )  A - kb + c! ;
 
-: +kb ( color c -- )
-    over green = if kb! else
-    over yellow = ( but don't overwrite a green...)
-    over kb@ green <> and if kb! else 2drop then then ;
+\ update kb with latest guess & score
+: >k ( color -- n )  dup green = if drop 3 else yellow = negate 1+ then ;
+: +kb ( color c -- )  swap >k swap  A - kb + 2dup c@ > if c! else 2drop then ;
+: update-kb  score  guess len bounds do  count i c@ +kb  loop drop ;
 
-: mark-kb  score  guess len bounds do  count i c@ +kb  loop drop ;
-
-: NEW  new-game  clear-history  kb 26 grey fill ;
-
-: G ( "w" -- ) \ "G RAISE" etc.
-    w pad w! pad len upper ( convenient )
-    pad score-word save-history    
-    cr 2 spaces score w. ." (" guesses @ 0 .r ." ) "
-    solved if ." You WIN! " else
-    failed if ." Better luck next time! " then then ;
+: NEW  new-game  clear-history  kb 26 erase ;
 
 : H .history ;
-: K 26 0 do i A + emit  i kb + c@
-    dup green = if drop [char] * else
-    yellow = if [char] ? else [char] - then then emit space loop ;
 
+: .k ( c n -- )  over kb@ = if dup emit space then drop ;
+: K cr ." Green:  " A 26 bounds do i 3 .k loop
+    cr ." Yellow: " A 26 bounds do i 2 .k loop
+    cr ." Grey:   " A 26 bounds do i 1 .k loop
+    cr ." Unused: " A 26 bounds do i 0 .k loop ;
+
+: G ( "w" -- ) \ "G RAISE" etc.
+    w pad w! pad dup len upper ( convenient )
+    score-word save-history update-kb .history
+    solved if ." You WIN! " else
+    failed if ." Better luck next time (" secret len type ." )" else 
+    k then then ;
